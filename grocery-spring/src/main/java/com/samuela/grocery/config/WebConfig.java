a9594cafd95161a85bb15ec7d34962327c622412
Samuela Abigail <94887862+Samuela31@users.noreply.github.com>
2025-10-28 22:12:48 +0530

    Update WebConfig.java

--- grocery-spring/src/main/java/com/samuela/grocery/config/WebConfig.java
+++ grocery-spring/src/main/java/com/samuela/grocery/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/api/**")
-		                .allowedOrigins("http://localhost:4200")
+		                .allowedOrigins("http://18.222.206.7") /* http://localhost:4200 */
 		                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
