6b7decfa0e873750e4ed73f0ae6a3c99aed7bcb3
Samuela Abigail <94887862+Samuela31@users.noreply.github.com>
2025-10-29 00:14:48 +0530

    Update environment.ts

--- grocery-angular/src/environment/environment.ts
+++ grocery-angular/src/environment/environment.ts
@@ -1,5 +1,6 @@
 // for local development
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:55420/grocery-project/api'
+  //apiUrl: 'http://localhost:55420/grocery-project/api'
+  apiUrl: 'http://18.222.206.7:55420/grocery-project/api' // actually don't put it here, it should be in prod.ts
 };
