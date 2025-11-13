384b247d40d187bb9a4cd03aa50ea06ac78a6e8e
Samuela Abigail <94887862+Samuela31@users.noreply.github.com>
2025-11-13 12:56:17 +0530

    Change API URL in environment.ts
    Updated the API URL for local development.

--- grocery-angular/src/environment/environment.ts
+++ grocery-angular/src/environment/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:55420/grocery-project/api',
-  apiUrl: 'http://3.129.7.191:55420/grocery-project/api', // actually don't put it here, it should be in prod.ts
+  apiUrl: 'http://13.59.46.12:55420/grocery-project/api', // actually don't put it here, it should be in prod.ts
 };
